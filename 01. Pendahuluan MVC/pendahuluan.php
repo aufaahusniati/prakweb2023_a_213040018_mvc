a0f8b0062864ee368c82f553a1b725b9341e765a
aufaahusniati <aufaahsnt21@gmail.com>
2023-10-23 11:44:12 +0700

    Menyelesaikan latihan video MVC di Pertemuan 6

---
+++ 01. Pendahuluan MVC/pendahuluan.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+MVC (Model-View-Controller)
+- "Pola arsitektur pada perancangan perangkat lunak berorientasi objek"
+- Memisahkan antaran tampilan, data, dan proses
+- Model (Data) : Representasi pengetahuan, mengelola data, logika bisnis 
+- Controller (Proses) : Perantara antara model dan view, menangani pemrosesan pada aplikasi, menangani aksi dari user
+- View (Tampilan) : Output apa yang dilihat oleh user, representasi visual dari model, lapisan representasi
+
+Kenapa MVC?
+- Organisasi dan struktur kode
+- Pemisahan logic dan tampilan
+- Perawatan kode
+- Implementasi konsep OOP yang sudah dipelajari
+- Digunakan oleh banyak web application framework
+
+Framework MVC : PHP (codelgniter, laravel, yii, dll)
+
+/*
+?>
---
+++ app/models/User_model.php
@@ -0,0 +1,12 @@
+<?php 
+
+class User_model {
+    private $nama = 'Aufaa';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
+
+?>
